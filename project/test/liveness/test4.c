b9d2875856e3175fcedb2c08247dde3ae35f2b02
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2014-03-31 16:29:36 +0530

    Got some dumps.

--- project/test/liveness/test4.c
+++ project/test/liveness/test4.c
@@ -2,15 +2,24 @@
 int a, b, c, *e;
 void p()
 {
-        printf ("%d", e);
+	printf ("%d", e);
 }
 int main()
 {
+	scanf("%d %d %d", &a, &b, &c);
 
-        if (a == b)
-                e = &c;   /* statement n1 */
-        else
-                e = &b;   /* statement n2 */
-        e = &a;           /* statement n3 */
-        p();
+	if (a == b)
+		e = &c;   /* statement n1 */
+	else
+		e = &b;   /* statement n2 */
+
+	if (c - 5)
+		a = b;
+	else if (c)
+		a = b - 5;
+	else
+		a = b + 5;
+
+	e = &a;           /* statement n3 */
+	p();
 }
